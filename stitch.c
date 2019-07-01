110f3795ec04d0e35806f51079d1fcb00fa3b58b
John Li <johnli0135@gmail.com>
2019-07-01 14:50:30 -0700

    Uncomment stab printing in stitch.c

--- stitch.c
+++ stitch.c
@@ -14,9 +14,9 @@ void main(int argc, char **argv) {
 
   yyparse(&e, &stab, &a);
   node_pp(stab, stdout, e);
-  //printf("vec_len(stab.strs) = %d\n", vec_len(stab.strs));
-  //for (int i = 0; i < vec_len(stab.strs); ++i)
-  //  printf("stab.strs[%d] = %s\n", i, (char *)stab.strs[i]);
+  printf("vec_len(stab.strs) = %lu\n", vec_len(stab.strs));
+  for (int i = 0; (size_t)i < vec_len(stab.strs); ++i)
+    printf("stab.strs[%d] = %s\n", i, (char *)stab.strs[i]);
   //check_types(stab, e);
 
   arena_del(a);
