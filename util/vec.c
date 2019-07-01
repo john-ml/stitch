100bf88f16a926f6c49da50f1a3ae3ea60b24eff
John Li <johnli0135@gmail.com>
2019-07-01 10:25:31 -0700

    Begin rewriting vec

--- util/vec.c
+++ util/vec.c
@@ -1,12 +1,15 @@
 #include "vec.h"
+#include <assert.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 // Stuff length / capacity information in before the 'actual vector'
 typedef struct raw_t {
+  int wid;
   int len;
   int cap;
-  any_t data[0];
+  void data[0];
 } *raw_t;
 
 // Convert to and from raw_t and vec_t
@@ -15,72 +18,75 @@ raw_t to_raw(vec_t v) {
   return (raw_t)((char *)v - offsetof(struct raw_t, data)); 
 }
 
-vec_t vec_create(int cap) {
-  raw_t v = malloc(sizeof(*v) + cap*sizeof(any_t));
+vec_t vec_create(arena_t *a, int wid, int cap) {
+  raw_t v = arena_alloc(a, sizeof(*v) + wid*cap);
+  v->wid = 0;
   v->len = 0;
   v->cap = cap;
   return of_raw(v);
 }
 
-vec_t vec_new() { return vec_create(8); }
+vec_t vec_new(arena_t *a, int wid) { return vec_create(a, wid, 8); }
 
-void vec_del(vec_t v, del_t f) {
-  raw_t w = to_raw(v);
-  for (int i = w->len; i--;)
-    f(v[i]);
-  free(w);
-}
-
-vec_t vec_sing(any_t x) {
+vec_t vec_sing(arena_t *a, int wid, void *x) {
   vec_t v = vec_new();
-  vec_add(&v, x);
+  vec_add(a, &v, x);
   return v;
 }
 
+int vec_wid(vec_t v) { return to_raw(v)->wid; }
 int vec_len(vec_t v) { return to_raw(v)->len; }
 int vec_cap(vec_t v) { return to_raw(v)->cap; }
 
-void vec_realloc(vec_t *v) { 
+void vec_realloc(arena_t *a, vec_t *v) { 
   raw_t w = to_raw(*v);
-  w = realloc(w, sizeof(*w) + w->cap*sizeof(any_t));
+  raw_t u = arena_alloc(a, sizeof(*u) + w->wid*w->cap);
+  memcpy(u, w, sizeof(*w) + w->wid*w->len);
   *v = of_raw(w);
 }
 
-void vec_expand(vec_t *v) { to_raw(*v)->cap *= 2; vec_realloc(v); }
-void vec_contract(vec_t *v) { to_raw(*v)->cap /= 2; vec_realloc(v); }
+// // Force v[i] = x, expanding v as needed
+// // Return old value of v[i] in out
+// void vec_put(arena_t *a, vec_t *v, int i, void *x, void *out);
+// 
+// // Same as vec_put, but initialize any blank entries to null
+// void vec_put_(arena_t *a, vec_t *v, int i, void *x, void *out, void *null);
 
-void vec_add(vec_t *v, any_t x) {
+void vec_expand(vec_t *v) { to_raw(*v)->cap *= 2; vec_realloc(v); }
+
+void vec_add(arena_t *a, vec_t *v, void *x) {
   if (vec_len(*v) == vec_cap(*v))
     vec_expand(v);
   raw_t w = to_raw(*v);
-  w->data[w->len++] = x;
+  ++w->len;
+  memcpy((char *)*v + w->wid*w->len, x, w->wid);
 }
 
-any_t vec_pop(vec_t *v) {
-  if (vec_len(*v) < vec_cap(*v) / 4)
-    vec_contract(v);
+void vec_pop(arena_t *a, vec_t *v, void *out) {
   raw_t w = to_raw(*v);
-  return w->data[--w->len];
+  assert(w->len > 0);
+  --w->len;
+  memcpy(out, (char *)*v + w->wid*w->len, w->wid);
 }
 
-any_t vec_put(vec_t *v, int i, any_t x) {
-  if (i >= to_raw(*v)->cap) {
-    to_raw(*v)->cap = i + 1;
-    vec_realloc(v);
-  }
-  raw_t w = to_raw(*v);
-  any_t old = w->data[i];
-  w->data[i] = x;
-  if (i >= w->len)
-    w->len = i + 1;
-  return old;
-}
-
-any_t vec_put_(vec_t *v, int i, any_t x, any_t null) {
-  int old_len = vec_len(*v);
-  any_t res = vec_put(v, i, x);
-  if (old_len < vec_len(*v));
-  for (int i = old_len; i < vec_len(*v) - 1; ++i)
-    (*v)[i] = null;
-  return res;
-}
+// any_t vec_put(arena_t *a, vec_t *v, int i, void *x, void *out) {
+//   if (i >= to_raw(*v)->cap) {
+//     to_raw(*v)->cap = i + 1;
+//     vec_realloc(v);
+//   }
+//   raw_t w = to_raw(*v);
+//   any_t old = w->data[i];
+//   w->data[i] = x;
+//   if (i >= w->len)
+//     w->len = i + 1;
+//   return old;
+// }
+// 
+// any_t vec_put_(vec_t *v, int i, any_t x, any_t null) {
+//   int old_len = vec_len(*v);
+//   any_t res = vec_put(v, i, x);
+//   if (old_len < vec_len(*v));
+//   for (int i = old_len; i < vec_len(*v) - 1; ++i)
+//     (*v)[i] = null;
+//   return res;
+// }
--- util/vec.h
+++ util/vec.h
@@ -1,22 +1,26 @@
-// Non-owning resizable array
+// Resizable array
 #ifndef VEC_INCLUDED_H
 #define VEC_INCLUDED_H
 
-#include "misc.h"
+#include "arena.h"
 #include <stddef.h>
 
 // A vector 'magically' behaves like a normal pointer
 // For any vec_t v, v[i] = ith element of v
-typedef any_t *vec_t;
+typedef void *vec_t;
 
 // Create a vector with the given initial capacity
-vec_t vec_create(int cap);
+// holding types of the given size
+vec_t vec_create(arena_t *a, int wid, int cap);
 
 // []
-vec_t vec_new();
+vec_t vec_new(arena_t *a, int wid);
 
 // [x]
-vec_t vec_sing(any_t x);
+vec_t vec_sing(arena_t *a, int wid, void *x);
+
+// The size (in bytes) of each element in v
+int vec_wid(vec_t v);
 
 // Number of elements in v
 int vec_len(vec_t v);
@@ -28,82 +32,70 @@ int vec_cap(vec_t v);
 // different location during resizing
 
 // Add to back
-void vec_add(vec_t *v, any_t x);
+void vec_add(arena_t *a, vec_t *v, void *x);
 
 // Remove from back
-any_t vec_pop(vec_t *v);
+void vec_pop(arena_t *a, vec_t *v, void *out);
 
 // Force v[i] = x, expanding v as needed
-// Return old value of v[i]
-any_t vec_put(vec_t *v, int i, any_t x);
+// Return old value of v[i] in out
+void vec_put(arena_t *a, vec_t *v, int i, void *x, void *out);
 
 // Same as vec_put, but initialize any blank entries to null
-any_t vec_put_(vec_t *v, int i, any_t x, any_t null);
+void vec_put_(arena_t *a, vec_t *v, int i, void *x, void *out, void *null);
 
-// f frees items
-void vec_del(vec_t v, del_t f);
+#define VEC(ty) ty *
 
-#define VEC_FOR(x, v, i, n) \
+#define VEC_FOR(t, x, v, i, n) \
+  t x; \
   for ( \
-    any_t \
-      i = (any_t)0, \
-      n = (any_t)(size_t)vec_len(v), \
-      x; \
-    x = (v)[(size_t)i], (size_t)i < (size_t)n; \
+    int i = 0, n = vec_len(v); \
+    x = (v)[i], i < n; \
     ++i)
 
-#define VEC_REV_FOR(x, v, i) \
-  for ( \
-    any_t \
-      i = (any_t)(size_t)vec_len(v), \
-      x; \
-    x = (v)[(size_t)i-1], (size_t)i; \
-    --i)
+#define VEC_REV_FOR(t, x, v, i) \
+  t x; \
+  for (int i = vec_len(v); x = (v)[i-1], i; --i)
 
-#define VEC_FOREACH(x, v) \
-  VEC_FOR(x, v, \
+#define VEC_FOREACH(t, x, v) \
+  VEC_FOR(t, x, v, \
     __VEC_FOREACH_i##__LINE__, \
     __VEC_FOREACH_n##__LINE__)
 
-#define VEC_REV_FOREACH(x, v) \
-  VEC_REV_FOR(x, v, \
+#define VEC_REV_FOREACH(t, x, v) \
+  VEC_REV_FOR(t, x, v, \
     __VEC_REV_FOREACH_i##__LINE__)
 
-#define VEC_FOR2(x1, v1, i1, n1, x2, v2, i2, n2) \
+#define VEC_FOR2(t1, x1, v1, i1, n1, t2, x2, v2, i2, n2) \
+  t1 x1; t2 x2; \
   for ( \
-    any_t \
-      i1 = (any_t)0, \
-      i2 = (any_t)0, \
-      n1 = (any_t)(size_t)vec_len(v1), \
-      n2 = (any_t)(size_t)vec_len(v2), \
-      x1, x2; \
-    x1 = (v1)[(size_t)i1], \
-      x2 = (v2)[(size_t)i2], \
-      (size_t)i1 < (size_t)n1 && (size_t)i2 < (size_t)n2; \
+    int \
+      i1 = 0, i2 = 0, \
+      n1 = vec_len(v1), n2 = vec_len(v2); \
+    x1 = (v1)[i1], x2 = (v2)[i2], \
+      i1 < n1 && i2 < n2; \
     ++i1, ++i2)
 
-#define VEC_REV_FOR2(x1, v1, i1, x2, v2, i2) \
+#define VEC_REV_FOR2(t1, x1, v1, i1, t2, x2, v2, i2) \
+  t1 x1; t2 x2; \
   for ( \
-    any_t \
-      i1 = (any_t)(size_t)vec_len(v1), \
-      i2 = (any_t)(size_t)vec_len(v2), \
-      x1, x2; \
-    x1 = (v1)[(size_t)i1-1], \
-      x2 = (v2)[(size_t)i2-1], \
-      (size_t)i1 && (size_t)i2; \
+    int \
+      i1 = vec_len(v1), i2 = vec_len(v2), \
+    x1 = (v1)[i1-1], x2 = (v2)[i2-1], \
+      i1 && i2; \
     --i1, --i2)
 
-#define VEC_FOREACH2(x1, v1, x2, v2) \
-  VEC_FOR2(x1, v1, \
+#define VEC_FOREACH2(t1, x1, v1, t2, x2, v2) \
+  VEC_FOR2(t1, x1, v1, \
     __VEC_FOREACH_i1##__LINE__, \
     __VEC_FOREACH_n1##__LINE__, \
-    x2, v2, \
+    t2, x2, v2, \
     __VEC_FOREACH_i2##__LINE__, \
     __VEC_FOREACH_n2##__LINE__)
 
-#define VEC_REV_FOREACH2(x1, v1, x2, v2) \
+#define VEC_REV_FOREACH2(t1, x1, v1, t2, x2, v2) \
   VEC_REV_FOR2( \
-    x1, v1, __VEC_REV_FOREACH_i1##__LINE__, \
-    x2, v2, __VEC_REV_FOREACH_i2##__LINE__)
+    t1, x1, v1, __VEC_REV_FOREACH_i1##__LINE__, \
+    t2, x2, v2, __VEC_REV_FOREACH_i2##__LINE__)
 
 #endif // VEC_INCLUDED_H
